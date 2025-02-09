1ac1e30f532684cd60a11889e68f20b1339e5815
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-09 12:02:59 -0500

    Order Details

--- cc_5.js
+++ cc_5.js
@@ -12,3 +12,20 @@ console.log(`Age: ${customer.age}`);
 console.log(`Email: ${customer.email}`);
 
 // Logged using template literals
+
+// Task 2: Order Details
+
+let order = {
+    orderId: 003,
+    totalAmount: 82,
+    status: "Shipment in Process",
+    displayOrder: function () {
+        console.log(`Order ID: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`);
+    }
+};
+// Declared order details with this function
+
+
+order.displayOrder();
+
+// Display details provided
