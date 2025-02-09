04f1baaae52c26e289206156c6b7ff7ba2933272
henriquesenaribeiro <henriqueribeiro@usf.edu>
2025-02-09 12:01:48 -0500

    Customer Profile

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,14 @@
+// Task 1: Customer Profile
 
+let customer = {
+    name: "Horacio Ribeiro",
+    age: 36,
+    email: "paudeoculos@usf.edu"
+};
+// Declared a customer 
+
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
+
+// Logged using template literals
